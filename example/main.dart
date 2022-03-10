541824fc7bd66ca23a55f7a8c1e22a5064729473
Piotr Rozpończyk <rozpo.dev@gmail.com>
2022-03-10 18:19:33 +0000

    Add example code for package usage

---
+++ example/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:platform_text/platform_text.dart';
+
+void main(List<String> args) {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: PlatformText('Hello Mom!'),
+        ),
+      ),
+    );
+  }
+}
