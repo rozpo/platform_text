36c96cf66685c512bf397a9152dc74375cb783dc
Piotr Rozpończyk <rozpo.dev@gmail.com>
2022-03-10 15:55:59 +0000

    Add widget smoke test

--- test/platform_text_test.dart
+++ test/platform_text_test.dart
@@ -1,10 +1,36 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:platform_text/platform_text.dart';
 
 void main() {
-  test('Check PlatformText data value', () {
-   const PlatformText platformText = PlatformText('test_value');
+  group('Smoke tests', () {
+    test('Check PlatformText data value', () {
+      // given
+      const String value = 'text_value';
 
-   expect(platformText.data, 'test_value');
+      // when
+      PlatformText platformText = const PlatformText(value);
+
+      // then
+      expect(platformText.data, value);
+    });
+
+    testWidgets('Check build method',
+        (WidgetTester tester) async {
+      // given
+      const String value = 'text_value';
+      PlatformText platformText = const PlatformText(value);
+
+      // when
+      await tester.pumpWidget(
+        Directionality(
+          textDirection: TextDirection.ltr,
+          child: platformText,
+        ),
+      );
+
+      // then
+      expect(find.widgetWithText(PlatformText, value), findsOneWidget);
+    });
   });
 }
